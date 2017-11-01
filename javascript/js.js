b5b0124e5095028c2a07b26ee128a8935e429662
Jessica Tran <traan.jessicaa@gmail.com>
2017-10-31 22:37:14 -0700

    can move from page to page

---
+++ javascript/js.js
@@ -0,0 +1,15 @@
+$('#back-join').click(function(){
+  window.location.href = './index.html';
+});
+
+$('#back').click(function(){
+  window.location.href = './dashboard.html';
+});
+
+$('#setting').click(function(){
+  window.location.href = './settings.html';
+});
+
+$('#help').click(function(){
+  window.location.href = './help.html';
+});
